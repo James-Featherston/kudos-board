07fc3c1768ddde989f58e427c95978eea70fc083
James Graham Featherston <jgfeatherston@meta.com>
2025-06-16 12:49:28 -0700

    Renamed frontend folder, basic layout of boards page

---
+++ frontend/src/pages/Board.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./Board.css";
+
+const boards = [
+  { id: 1, title: "hello", type: "celebration" },
+  { id: 2, title: "hello", type: "thank you" },
+  { id: 3, title: "hello", type: "Inspiration" },
+];
+
+const cards_temp = [
+  { id: 1, title: "title", descrition: "Description", votes: 0 },
+  { id: 2, title: "title", descrition: "Description", votes: 0 },
+];
+
+const Board = () => {
+  const [cards, setCards] = useState(null);
+  const [board, setBoard] = useState(null);
+  const { id } = useParams();
+  if (cards === null || board === null) {
+    return;
+  }
+
+  const getBoard = () => {
+    setBoard(boards.find((board) => board.id === parseInt(id)));
+  };
+  const getCards = () => {
+    setCards(cards_temp);
+  };
+
+  useEffect(() => {
+    getBoard();
+    getCards();
+  }, []);
+  return (
+    <div className="board-container">
+      <h2>{board?.title} </h2>
+      <button>Create a Card</button>
+      {cards.map}
+    </div>
+  );
+};
+
+export default Board;
